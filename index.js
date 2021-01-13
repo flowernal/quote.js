0cc07dddacd9db1a8a84e085d8f31277ebe4321e
BramboraSK <30873527+BramboraSK@users.noreply.github.com>
2021-01-13 10:03:48 +0100

    Fix indentation

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 module.exports = (amount = 1, options) => {
     let { quotes } = require("./quotes");
 
-	if(options.minLength) quotes = quotes.filter(e => e.quoteText.length >= options.minLength);
+    if(options.minLength) quotes = quotes.filter(e => e.quoteText.length >= options.minLength);
     if(options.maxLength) quotes = quotes.filter(e => e.quoteText.length <= options.maxLength);
     if(options.authorIsKnown) quotes = quotes.filter(e => e.quoteAuthor.length > 0);
     if(options.authorIsKnown === false) quotes = quotes.filter(e => e.quoteAuthor.length === 0);
